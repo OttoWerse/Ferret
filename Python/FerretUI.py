bf4dc459c22ad16fd6ed0c39ce5c690706304cd8
Otto Werse <otto.werse@web.de>
2020-06-25 00:39:14 +0200

    Mayor refactoring and some more comments

--- Python/FerretUI.py
+++ Python/FerretUI.py
@@ -1,63 +1,84 @@
 from tkinter import *
 
-i = 1
+# Initialize row counter
+current_row = 1
+
 
 def GUI(list):
-    Boxwurst = []
-    global i
-    i = 1
+    # Fix pythons weirdness with namespaces
+    global current_row
+    current_row = 1
+    # Create a list for tuples of entry fields
+    tuples = []
 
-    def addBox(key="", value=""):
-        global i
-        ent = Entry(root)
-        ent.insert(END, key)
-        ent2 = Entry(root)
-        ent2.insert(END, value)
-        Boxwurst.append((ent, ent2))
-        if i > 1:
 
-            def delBox():
-                Boxwurst.remove((ent, ent2))
-                ent.destroy()
-                ent2.destroy()
-                but.destroy()
+    def add_row(key="", value=""):
+        # Fix pythons weirdness with namespaces
+        global current_row
 
-            but = Button(root, text="-", command=delBox)
+        # Create entry field for the key
+        key_entry_field = Entry(root)
+        key_entry_field.insert(END, key)
+        key_entry_field.grid(row=current_row)
+        # Create entry field for the value
+        value_entry_field = Entry(root)
+        value_entry_field.insert(END, value)
+        value_entry_field.grid(row=current_row, column=1)
+        # Add entry field references to list
+        tuples.append((key_entry_field, value_entry_field))
+
+        # If the current row is not the first one
+        if current_row > 1:
+            # Create a function to delete the current row of a button
+            def delete_row():
+                tuples.remove((key_entry_field, value_entry_field))
+                key_entry_field.destroy()
+                value_entry_field.destroy()
+                row_button.destroy()
+            # Assign the function to the button in that row
+            row_button = Button(root, text="-", command=delete_row)
         else:
-            but = Button(root, text="+", command=addBox)
-        ent.grid(row=i)
-        ent2.grid(row=i, column=1)
-        but.grid(row=i, column=2)
-        i = i + 1
+            # Assign the function to add a row (only for the first button)
+            row_button = Button(root, text="+", command=add_row)
 
-    def boxen():
-        for x in Boxwurst:
-            Boxt0 = x[0].get()
-            Boxt1 = x[1].get()
-            if Boxt0 and Boxt1:
-                # print(Boxt0, Boxt1)
-                list[Boxt0] = Boxt1
-        # print(list)
+        row_button.grid(row=current_row, column=2)
 
+        # Increase the row counter
+        current_row = current_row + 1
+
+    def build_dict():
+        # Clear the list (in order to sync deletions)
+        list.clear()
+        # Iterate through all tuples
+        for tuple in tuples:
+            # Get the key and value from the entry fields in the tuple
+            key = tuple[0].get()
+            value = tuple[1].get()
+            # if they are not empty
+            if key and value:
+                # add an entry to the dict
+                list[key] = value
+
+    # Initialise Tk
     root = Tk()
+
+    # If the passed list is not empty
     if len(list) > 0:
+        # Add all list items as rows
         for key in list:
-            addBox(key, list[key])
+            add_row(key, list[key])
     else:
-        addBox()
+        # Add a single, empty, row
+        add_row()
 
-    # e1 = Entry(root)
-    # e2 = Entry(root)
-    # button = Button(root, text="+", command=addBox)
-    benjamin = Button(root, text="Ausgabe", command=boxen)
-    # e1.grid(column=0, row=1)
-    # e2.grid(column=1, row=1)
-    # button.grid(column=2, row=1)
-    benjamin.grid(column=0, row=0)
-    # Boxwurst.append((e1, e2))
+    # Add a button to safe the current rows
+    commit_button = Button(root, text="Ausgabe", command=build_dict)
+    commit_button.grid(column=0, row=0)
 
+    # Start the Tk loop
     root.mainloop()
 
 
 if __name__ == "__main__":
+    # A small test case
     GUI({"hello": "world"})
